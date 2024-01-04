9bd6e1c621b3a94396faf4873de6ce8e5def6194
mciancone <mathieu@wikit.ai>
2024-01-04 15:58:45 +0100

    add batch size management

--- script_mteb_french/run_benchmark.py
+++ script_mteb_french/run_benchmark.py
@@ -12,8 +12,8 @@ benchmark_is_ready = False
 # Step 1 : Setup model list #
 #############################
 SENTENCE_TRANSORMER_MODELS = [
-    "camembert/camembert-base",
-    "camembert/camembert-large",
+    #"camembert/camembert-base",
+    #"camembert/camembert-large",
     "bert-base-multilingual-cased",
     "bert-base-multilingual-uncased",
     "flaubert/flaubert_base_uncased",
@@ -36,19 +36,23 @@ SENTENCE_TRANSORMER_MODELS = [
     "Geotrend/bert-base-15lang-cased",
     "Geotrend/bert-base-10lang-cased",
     "shibing624/text2vec-base-multilingual",
+    "izhx/udever-bloom-560m",
+    "izhx/udever-bloom-1b1",
+    "izhx/udever-bloom-3b",
+    "izhx/udever-bloom-7b1"
 ]
 
 
 LASER_MODELS = ["Laser2"]
 
-VOYAGE_MODELS = ["voyage-lite-01"]
+VOYAGE_MODELS = ["voyage-lite-01", "voyage-01"]
 
 OPEN_AI_MODELS = ["text-embedding-ada-002"]
 
 ########################
 # Step 2 : Setup tasks #
 ########################
-TASKS = ["SyntecRetrieval"]
+TASKS = ["SyntecRetrieval", "AlloprofRetrieval"]
 
 ##########################
 # Step 3 : Run benchmark #
@@ -65,13 +69,7 @@ if benchmark_is_ready:
     MODELS.extend([ModelConfig(name, model_type="open_ai") for name in OPEN_AI_MODELS])
     MODELS.extend([ModelConfig(name, model_type="laser") for name in LASER_MODELS])
 else:
-    MODELS = [
-        # ModelConfig("text-embedding-ada-002", model_type="open_ai"),
-        # ModelConfig("voyage-lite-01", model_type="voyage_ai"),
-        ModelConfig(
-            "sentence-transformers/all-MiniLM-L6-v2", model_type="sentence_transformer"
-        )
-    ]
+    MODELS = [ModelConfig(name, model_type="voyage_ai") for name in VOYAGE_MODELS]
 
 
 def parse_args() -> argparse.Namespace:
@@ -93,6 +91,7 @@ def main(args):
         for task in TASKS:
             # change the task in the model config ! This is important to specify the chromaDB collection !
             model_name = model_config.model_name
+            model_config.batch_size = args.batchsize
             print("Running task: ", task, "with model", model_name)
             eval_splits = ["validation"] if task == "MSMARCO" else ["test"]
             evaluation = MTEB(tasks=[task], task_langs=[args.lang])
