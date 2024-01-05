958336d073fd65a2be03705dec8e3be19d3500b1
mciancone <mathieu@wikit.ai>
2024-01-05 10:33:24 +0100

    cleaning of tasks

--- script_mteb_french/preload_tasks.py
+++ script_mteb_french/preload_tasks.py
@@ -26,16 +26,22 @@ TASK_LIST_PAIR_CLASSIFICATION = [
     "OpusparcusPC",
 ]
 
-TASK_LIST_RERANKING = ["SyntecReranking", "AlloprofReranking"]
-
-TASK_LIST_RETRIEVAL = [
-    "AlloprofRetrieval",
-    "BSARDRetrieval",
-    "HagridRetrieval",
-    "SyntecRetrieval",
+TASK_LIST_RERANKING = [
+    "SyntecReranking",
+    "AlloprofReranking"
 ]
 
-TASK_LIST_STS = ["STSBenchmarkMultilingualSTS", "STS22"]
+TASK_LIST_RETRIEVAL = [
+    "AlloprofRetrieval", 
+    "BSARDRetrieval", 
+    "SyntecRetrieval"
+]
+
+TASK_LIST_STS = [
+    "STSBenchmarkMultilingualSTS",
+    "STS22",
+    "SICKFr"
+]
 
 TASK_LIST_SUMMARIZATION = [
     "SummEvalFr",
@@ -57,13 +63,6 @@ TASK_LIST = (
     + TASK_LIST_SUMMARIZATION
     + TASK_LIST_BITEXTMINING
 )
-
-os.environ["HF_DATASETS_OFFLINE"] = "0"  # 1 for offline
-os.environ["TRANSFORMERS_OFFLINE"] = "0"  # 1 for offline
-os.environ["TRANSFORMERS_CACHE"] = "C:/Users/mathi/.cache/huggingface/models"
-os.environ["HF_DATASETS_CACHE"] = "C:/Users/mathi/.cache/huggingface/datasets"
-os.environ["HF_MODULES_CACHE"] = "C:/Users/mathi/.cache/huggingface/modules"
-os.environ["HF_METRICS_CACHE"] = "C:/Users/mathi/.cache/huggingface/metrics"
 
 evaluation = MTEB(tasks=TASK_LIST, task_langs=["fr"])
 
