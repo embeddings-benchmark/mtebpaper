ac68f9b5ee564ceb4839668d6494d35bb3843236
wissam-sib <36303760+wissam-sib@users.noreply.github.com>
2024-01-09 11:45:44 +0100

    Improving get model specs script (#8)
    * improving get model specs script
    * fix conversion bytes to gb

--- script_mteb_french/get_model_specs.py
+++ script_mteb_french/get_model_specs.py
@@ -3,17 +3,19 @@ from typing import Dict
 import sys
 
 from huggingface_hub import HfFileSystem
-from sentence_transformers import SentenceTransformer
+import model_spec_utils
 
 """
 This script's purpose is to get the specifications of models in terms of :
-- Size (in GB)
-- number of parameters
-- input size (number of tokens)
+- Computed size with parameters as float32 (in GB)
+- Model files size (in GB)
+- Number of parameters
+- Input size (number of tokens)
 - Embedding size (vector dimension)
 
-Just specify the names of the HF models for which you want the specs,
-as a list of names named SENTENCE_TRANSFORMER_MODELS
+Just specify the names of the models for which you want the specs,
+as lists (SENTENCE_TRANSFORMER_MODELS, UDEVER_BLOOM_MODELS, TFHUB_MODELS, 
+LASER_MODELS)
 """
 
 SENTENCE_TRANSFORMER_MODELS = [
@@ -21,32 +23,55 @@ SENTENCE_TRANSFORMER_MODELS = [
     "dangvantuan/sentence-camembert-base",
 ]
 
+UDEVER_BLOOM_MODELS = [
+    'izhx/udever-bloom-560m'
+]
+
+TFHUB_MODELS = [
+    'https://tfhub.dev/google/universal-sentence-encoder-multilingual/3'
+]
+
+LASER_MODELS = [
+    'french'
+]
+
+GET_SPEC_FUNCTIONS = {
+    "sentence_transformers": "get_sentence_transformers_model_specs",
+    "udever_bloom": "get_udever_bloom_model_specs",
+    "tfhub": "get_tfhub_model_specs",
+    "laser": "get_laser_model_specs"
+}
 
 HFFS = HfFileSystem()
 
 
-def get_model_specs(model_name: str) -> Dict:
+def get_model_specs(model_name: str, model_type: str) -> Dict:
     """Get the specifications of a HF model
 
     Args:
         model_name (str): the hf name of the model
             (i.e. 'sentence-transformers/all-MiniLM-L6-v2')
+        model_type (str): the type of model (sentence_transformers, udever_bloom, tfhub, laser)
 
     Returns:
         Dict: the specifications of the model
     """
 
-    model = SentenceTransformer(model_name)
+    num_params, embedding_size, input_size = getattr(model_spec_utils, GET_SPEC_FUNCTIONS[model_type])(model_name)
+
+    model_size_float32_in_gb = num_params * 4 / 1e9
+    model_size_with_hf_filesystem = get_size_using_HF_filsystem(model_name) if model_type in ["sentence_transformers", "udever_bloom"] else None
     specs = {
-        "size_in_GB": get_size_using_HF_filsystem(model_name),
-        "n_params": model._parameters,  # TODO: Change with Wissam's method
-        "input_size": model._modules.get("0").max_seq_length,
-        "embedding_size": model.encode("dummy sentence").shape[0],
+        "computed_size_in_gb": model_size_float32_in_gb,
+        "files_size_in_gb": model_size_with_hf_filesystem,
+        "n_params": num_params,
+        "input_size": input_size,
+        "embedding_size": embedding_size,
     }
     return specs
 
 
-def get_size_of_object(model: SentenceTransformer, seen=None) -> float:
+def get_size_of_object(model, seen=None) -> float:
     """Recursively finds size of objects"""
     size = sys.getsizeof(model)
     if seen is None:
@@ -88,7 +113,13 @@ def get_size_using_HF_filsystem(model_name: str):
 
 SPECS = {}
 for model_name in SENTENCE_TRANSFORMER_MODELS:
-    SPECS[model_name] = get_model_specs(model_name)
+    SPECS[model_name] = get_model_specs(model_name, "sentence_transformers")
+for model_name in UDEVER_BLOOM_MODELS:
+    SPECS[model_name] = get_model_specs(model_name, "udever_bloom")
+for model_name in TFHUB_MODELS:
+    SPECS[model_name] = get_model_specs(model_name, "tfhub")
+for model_name in LASER_MODELS:
+    SPECS[model_name] = get_model_specs(model_name, "laser")
 
 with open("model_specs.json", "w") as f:
     json.dump(SPECS, f)
---
+++ script_mteb_french/model_spec_utils.py
@@ -0,0 +1,77 @@
+import tensorflow_text as text # Required to load muse models
+import tensorflow as tf
+import tensorflow_hub as hub
+import torch
+
+from sentence_transformers import SentenceTransformer
+from transformers import BloomModel, AutoTokenizer
+from laser_encoders import LaserEncoderPipeline
+
+
+def count_torch_model_params(model, all_params=True):
+    if all_params:
+        num_params = sum(p.numel() for p in model.parameters())
+    else:
+        # trainable params
+        num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    return num_params
+
+
+def get_sentence_transformers_model_specs(model_path, all_params=True):
+    model = SentenceTransformer(model_path)
+    num_params = count_torch_model_params(model, all_params)
+    embedding_size = model.encode("dummy sentence").shape[0]
+    input_size = model._modules.get("0").max_seq_length
+    return num_params, embedding_size, input_size
+
+
+def get_udever_bloom_model_specs(model_path, all_params=True):
+    model = BloomModel.from_pretrained(model_path)
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    boq, eoq, bod, eod = '[BOQ]', '[EOQ]', '[BOD]', '[EOD]'
+    eoq_id, eod_id = tokenizer.convert_tokens_to_ids([eoq, eod])
+    if tokenizer.padding_side != 'left':
+        print('!!!', tokenizer.padding_side)
+        tokenizer.padding_side = 'left'
+
+    def encode(texts: list, is_query: bool = True, max_length=300):
+        bos = boq if is_query else bod
+        eos_id = eoq_id if is_query else eod_id
+        texts = [bos + t for t in texts]
+        encoding = tokenizer(
+            texts, truncation=True, max_length=max_length - 1, padding=True
+        )
+        for ids, mask in zip(encoding['input_ids'], encoding['attention_mask']):
+            ids.append(eos_id)
+            mask.append(1)
+        inputs = tokenizer.pad(encoding, return_tensors='pt')
+        with torch.inference_mode():
+            outputs = model(**inputs)
+            embeds = outputs.last_hidden_state[:, -1]
+        return embeds
+
+    num_params = count_torch_model_params(model, all_params)
+    embedding_size = encode(["dummy sentence"]).shape[1]
+    input_size = model.config.seq_length
+    return num_params, embedding_size, input_size
+
+
+def get_tfhub_model_specs(model_path, all_params=True):
+    model_keras = hub.KerasLayer(model_path)
+    all_weights = model_keras.weights if all_params else model_keras.trainable_variables
+    num_params = sum(tf.size(p).numpy() for p in all_weights)
+    model = hub.load(model_path)
+
+    def embed(input):
+        return model(input)
+    embedding_size = embed("dummy sentence").shape[1]
+    input_size = None
+    return num_params, embedding_size, input_size
+
+
+def get_laser_model_specs(model_path, all_params=True):
+    encoder = LaserEncoderPipeline(lang=model_path)
+    num_params = count_torch_model_params(encoder.encoder.encoder, all_params)
+    embedding_size = encoder.encode_sentences(["dummy sentence"]).shape[1]
+    input_size = encoder.encoder.max_tokens
+    return num_params, embedding_size, input_size
